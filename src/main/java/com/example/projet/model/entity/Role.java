bf87f266d0fa06b3e3a389fce2a8021b5d8dfa71
Oumaima-BenElhajAli <oumaima.benelhadjali@gmail.com>
2022-05-03 22:21:25 +0100

    user auth

---
+++ src/main/java/com/example/projet/model/entity/Role.java
@@ -0,0 +1,19 @@
+package com.example.projet.model.entity;
+import lombok.*;
+import javax.persistence.*;
+@Entity
+@Table(name = "ROLES")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+}
